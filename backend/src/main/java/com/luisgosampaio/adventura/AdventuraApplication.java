79cb9c8e84132bf46fc48ce9d7b8321bcf2a6d9a
Guga2111 <luisgosampaio@gmail.com>
2026-02-09 21:34:06 -0300

    chore: starting springboot and react project

---
+++ backend/src/main/java/com/luisgosampaio/adventura/AdventuraApplication.java
@@ -0,0 +1,13 @@
+package com.luisgosampaio.adventura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventuraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luisgosampaio/adventura/AdventuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisgosampaio.adventura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
