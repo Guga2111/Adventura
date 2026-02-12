2414ca5ccfbaf18dcda6c7ad0d7b0124a0a5f445
Guga2111 <luisgosampaio@gmail.com>
2026-02-12 18:33:56 -0300

    fix: changing text coloring and adding navigation to register button

--- frontend/src/pages/LandingPage/LandingPageHeader.tsx
+++ frontend/src/pages/LandingPage/LandingPageHeader.tsx
@@ -1,23 +1,27 @@
 import { MapPinned } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
 
 export function LandingPageHeader() {
+
+    const navigate = useNavigate();
+
     return (
         <header className="flex items-center justify-between px-6 py-4 border-b">
             <a href="/" className="flex items-center gap-2 text-xl font-bold">
-                <MapPinned className="size-6" />
-                Adventura
+                <MapPinned className="size-7 text-[#dda600]" />
+                <span className="text-xl text-bold">Adventura</span>
             </a>
 
             <nav className="hidden md:flex items-center gap-6 text-sm font-medium text-muted-foreground">
-                <a href="#features" className="hover:text-foreground transition-colors">Features</a>
-                <a href="#how-it-works" className="hover:text-foreground transition-colors">How It Works</a>
-                <a href="#about" className="hover:text-foreground transition-colors">About</a>
+                <a href="#how-it-works" className="hover:text-foreground transition-colors">Como Funciona</a>
+                <a href="#about" className="hover:text-foreground transition-colors">Sobre</a>
+                <a href="#features" className="hover:text-foreground transition-colors">Funcionalidades</a>
             </nav>
 
             <div className="flex items-center gap-3">
-                <Button variant="ghost" size="sm" >Sign In</Button>
-                <Button size="sm">Sign Up</Button>
+                <Button variant="ghost" size="sm" className="cursor-pointer">Login</Button>
+                <Button size="sm" className="bg-gradient-primary text-white hover:opacity-90 cursor-pointer" onClick={() => navigate('/signup')} >Registrar</Button>
             </div>
         </header>
     )
